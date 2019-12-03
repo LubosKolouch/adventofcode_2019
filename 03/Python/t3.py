e0558891259f1f658511fb7b22b38d672812dcec
Lubos Kolouch <lubos@kolouch.net>
2019-12-03 20:37:23 +0100

    Cleanup Python code

--- 03/Python/t3.py
+++ 03/Python/t3.py
@@ -6,9 +6,15 @@ import re
 grid={}
 
 def process_step(posx,posy) :
-                if (grid[(posx,posy)] > 1) :
-                    distance = abs(posx) + abs(posy)
-                    print("intersection "+str(posx)+" "+str(posy)+" distance "+str(distance))
+                if (posx,posy) not in seen:
+                    if (posx,posy) not in grid:
+                        grid[(posx,posy)] = 1
+                    else:
+                        grid[(posx,posy)] += 1
+                        distance = abs(posx) + abs(posy)
+                        print("intersection "+str(posx)+" "+str(posy)+" distance "+str(distance))
+
+                seen[(posx,posy)]=1
 
 file = open("../input","r")
 
@@ -17,7 +23,7 @@ for line in file:
         posx=0
         posy=0
         seen={}
-       
+        
         for item in arr:
             match=re.match(r"(.)(.*)", item)
             direction=match.group(1)
@@ -26,50 +32,22 @@ for line in file:
             if (direction == 'R') :
                 for i in range(1,int(count)+1):
                     posx += 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
             
             if (direction == 'L') :
                 for i in range(1,int(count)+1):
                     posx -= 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
  
             if (direction == 'U') :
                 for i in range(1,int(count)+1):
                     posy += 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
 
 
             if (direction == 'D') :
                 for i in range(1,int(count)+1):
                     posy -= 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
 
 
--- 03/Python/t3_2.py
+++ 03/Python/t3_2.py
@@ -14,12 +14,19 @@ def process_step(posx,posy) :
                 global steps_count
                 global steps
                 global min_count
-
+                
                 steps_count += 1
+
+                if (posx,posy) not in seen:
+                        if (posx,posy) not in grid:
+                            grid[(posx,posy)] = 1
+                        else:
+                            grid[(posx,posy)] += 1
+
+                seen[(posx,posy)]=1
 
                 if (i,posx,posy) not in steps:
                     steps[(i,posx,posy)] = steps_count
-#                    print("posx "+str(posx)+" posy "+str(posy)+"grid value "+str(grid[(posx,posy)])+" i "+str(i))
 
                 if (grid[(posx,posy)] > 1) :
                     total_steps = steps[(1,posx,posy)] + steps[(2,posx,posy)]
@@ -33,6 +40,8 @@ def process_step(posx,posy) :
                         min_count = total_steps
 
                     print("min count "+str(min_count))
+
+
 
 file = open("../input","r")
 
@@ -52,50 +61,22 @@ for line in file:
             if (direction == 'R') :
                 for j in range(1,int(count)+1):
                     posx += 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
-            
+
             if (direction == 'L') :
                 for j in range(1,int(count)+1):
                     posx -= 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
  
             if (direction == 'U') :
                 for j in range(1,int(count)+1):
                     posy += 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
 
 
             if (direction == 'D') :
                 for j in range(1,int(count)+1):
                     posy -= 1
-
-                    if (posx,posy) not in seen:
-                        if (posx,posy) not in grid:
-                            grid[(posx,posy)] = 1
-                        else:
-                            grid[(posx,posy)] += 1
-                    seen[(posx,posy)]=1
                     process_step(posx,posy)
 
 
